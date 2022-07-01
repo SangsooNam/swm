4cbca9663044e8e6e258ad7a8cd45c96187bca85
Sangsoo Nam <sangsoo@spotify.com>
2022-07-01 14:34:39 +0200

    Print 'Hello World'

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  console.log("Hello World");
+  
   return (
     <div className={styles.container}>
       <Head>
